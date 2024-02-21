ef8ec3bb9fd31456654ca2bc542a091908ad7bde
Thomas McKeown <tmckeown@sandiego.edu>
2024-02-21 13:26:10 -0800

    code set up for further implementation. please read comments before writing anything

--- neuron.java
+++ neuron.java
@@ -21,11 +21,31 @@ public class neuron{
         }
     }
 
-    public void trainNeuron(int[] inputs, float answer, float alpha, float theta){
+    public boolean trainNeuron(int[] inputs, float answer, float alpha, float theta){
         float neurAns = 0;
+        int y;
+        boolean noChange = true;
         for (int i=0; i<inputs.length; i++){
             neurAns += inputs[i]*weights[i];
         }
-        
+        if (neurAns > theta){
+            y = 1;
+        }
+        else if (neurAns < theta){
+            y = -1;
+        }
+        else {
+            y = 0;
+        }
+        if (y != answer){
+            noChange = false;
+            for (int i=0; i<inputs.length; i++){
+                weights[i] = weights[i] + answer*inputs[i]*alpha;
+            }
+        }
+        return noChange;
     }
+    //Need to add a method that calculates and returns the answer (for non training)
+
+    //Need a method to return the weights for storage in a file
 }
