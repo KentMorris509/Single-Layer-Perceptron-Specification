a5be3f5ab879b8d45399cf9bbdb01157e1f48568
Thomas McKeown <tmckeown@sandiego.edu>
2024-02-21 13:26:10 -0800

    code set up for further implementation. please read comments before writing anything

---
+++ neuralNet.java
@@ -0,0 +1,8 @@
+public class neuralNet {
+    //initialize specified number of neurons in this class and create higher level
+    //training program.
+    //For actual tests, take results off all neurons and decide what result was
+    //basically ties together the individual neurons
+    //higher level class should handle receive output vector and interpret appropriately
+    //Goal of this class and the neuron is class is to not be tied to this specific assignment
+}
